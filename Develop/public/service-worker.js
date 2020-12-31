437d0edc1ddd72cccfc789ccc70e4ed57d9e5039
Josh Allan <allan.josh07@gmail.com>
2020-12-31 11:25:51 -0500

    Added IndexedDB code to enable offline/online events.

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -6,7 +6,8 @@ const FILES_TO_CACHE = [
   '/styles.css',
   '/manifest.webmanifest',
   '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png'
+  '/icons/icon-512x512.png',
+  '/db.js'
 ];
 
 const CACHE_NAME = 'static-cache-v2';
